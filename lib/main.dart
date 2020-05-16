2364737f046dd82d5c83de0821deea0faf90c1e3
Henry-Ning <henryhuning@gmail.com>
2020-05-16 01:37:14 -0700

    83. How to Play Multiple Sounds 9min

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,71 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              }, child: Text(
-              'Click Me'
-            ),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                 playSound(1);
+                },
+                child: Text('1'),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text('2'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                color: Colors.yellow,
+                child: Text('3'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                color: Colors.green,
+                child: Text('4'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                color: Colors.teal,
+                child: Text('5'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                color: Colors.blue,
+                child: Text('6'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                color: Colors.purple,
+                child: Text('7'),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
